8a16d63a9ccd321b49105efae9af2a94058749cc
Rafael Arias <rafael.ariasf03@gmail.com>
2025-04-06 19:13:18 -0700

    Ya funciona tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,24 @@
 import { Text, View } from "react-native";
-import "./global.css"; // Asegúrate de que Tailwind esté bien configurado en tu proyecto
+import "./global.css";
 import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-gray-100 p-4">
-    <Text className="text-red-600">
+    <Text className="text-accent">
           ¡Bienvenido esto es una prueba de tailwind!
         </Text>
         <Link href={'/onboarding'}>Onboarding</Link>
-        <Link href={'/movie/avengers'}>Avengers Movie</Link>
-        
+        {/* <Link href={'/movie/avengers'}>Avengers Movie</Link>
+         */}
     </View>
     
   );
   
 }
 
-
+// index
+// profile
+// save
+// search / buscar
+// {tabs} navegacion por enrutacion
